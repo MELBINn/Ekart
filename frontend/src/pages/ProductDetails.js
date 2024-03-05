36be70bde70758600f23c0e8eb4f9fc5c05e42c0
MELBINn <melbinsunny29@gmail.com>
2024-03-05 13:40:25 +0530

    added cart in details page

--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
 import axios from "axios";
 import { Url } from "../url";
-// import { useCart } from "../context/cart";
+import { useAuth } from "../context/auth";
+import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
 import { useParams, useNavigate } from "react-router-dom";
 const ProductDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
-  // const [cart, setCart] = useCart();
+  const [cart, setCart] = useCart();
   const [product, setProduct] = useState({});
   const [relatedProducts, setRelatedProducts] = useState([]);
+  const [auth] = useAuth();
+  const [products, setProducts] = useState([]);
+  
 
   //inital details
   useEffect(() => {
@@ -19,8 +23,8 @@ const ProductDetails = () => {
   //getProduct
   const getProduct = async () => {
     try {
-      const { data } = await axios.get(Url+
-        `/api/v1/product/get-product/${params.slug}`
+      const { data } = await axios.get(
+        Url + `/api/v1/product/get-product/${params.slug}`
       );
       setProduct(data?.product);
       getSimilarProduct(data?.product._id, data?.product.category._id);
@@ -31,12 +35,27 @@ const ProductDetails = () => {
   //get similar product
   const getSimilarProduct = async (pid, cid) => {
     try {
-      const { data } = await axios.get(Url+
-        `/api/v1/product/related-product/${pid}/${cid}`
+      const { data } = await axios.get(
+        Url + `/api/v1/product/related-product/${pid}/${cid}`
       );
       setRelatedProducts(data?.products);
     } catch (error) {
       console.log(error);
+    }
+  };
+  const AddToCart = async (productId, p) => {
+    const name = p.name;
+    const price = p.price;
+    const User = auth.user._id;
+    try {
+      const res = await axios.post(
+        Url + "/api/v1/cart/create",
+        { productid: productId, User: User, Name: name, Price: price },
+        { withCredentials: true }
+      );
+      toast.success("Item Added to cart");
+    } catch (err) {
+      console.error("Error in AddToCart:", err);
     }
   };
   return (
@@ -44,7 +63,7 @@ const ProductDetails = () => {
       <div className="row container mt-2">
         <div className="col-md-6">
           <img
-            src={Url+`/api/v1/product/product-photo/${product._id}`}
+            src={Url + `/api/v1/product/product-photo/${product._id}`}
             className="card-img-top"
             alt={product.name}
             height="300"
@@ -57,8 +76,16 @@ const ProductDetails = () => {
           <h6>Description : {product.description}</h6>
           <h6>Price : {product.price}</h6>
           <h6>Category : {product?.category?.name}</h6>
-          <button className="btn btn-secondary ms-1">ADD TO CART</button>
-          {/* Add the second "ADD TO CART" button here if needed */}
+
+          
+            <button
+              // <button className="btn btn-secondary ms-1">ADD TO CART</button>
+              className="btn btn-secondary ms-1"
+              onClick={() => AddToCart(product._id, product)} // Pass the product object
+            >
+              Add To Cart
+            </button>
+          
         </div>
       </div>
       <hr />
@@ -71,7 +98,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2 similar-products" key={p._id}>
               <img
-                src={Url+`/api/v1/product/product-photo/${p?._id}`}
+                src={Url + `/api/v1/product/product-photo/${p?._id}`}
                 className="card-img-top"
                 alt={p.name}
                 height={300} // Overridden by .similar-products-card-img-top
